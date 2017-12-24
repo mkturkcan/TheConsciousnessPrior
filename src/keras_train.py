c60ca21f937245b18e60383594bd6bfe85cda4df
Mehmet Kerem Turkcan <mkt2126@columbia.edu>
2017-12-24 00:09:29 -0500

    Updates and comments

--- src/keras_train.py
+++ src/keras_train.py
@@ -22,7 +22,7 @@ X = np.stack(X)
 X.shape
 
 def linear_objective(y_true, y_pred):
-    return -1 * K.mean(y_pred, axis=-1)
+    return 1.0 - 1.0 * K.mean(y_pred, axis=-1)
 
 ## Define Extra Layers to Use
 class CELayer(Layer):
@@ -31,7 +31,19 @@ class CELayer(Layer):
     def build(self, input_shape):
         super(CELayer, self).build(input_shape)
     def call(self, x):
-        return kullback_leibler_divergence(x[0], x[1])
+        return -1. * kullback_leibler_divergence(x[0], x[1])
+    def compute_output_shape(self, input_shape):
+        return (None, X.shape[1]-1)
+
+class CPLayer(Layer):
+    def __init__(self, **kwargs):
+        super(CPLayer, self).__init__(**kwargs)
+    def build(self, input_shape):
+        super(CPLayer, self).build(input_shape)
+    def call(self, x):
+            y_true = K.l2_normalize(x[0], axis=-1)
+            y_pred = K.l2_normalize(x[1], axis=-1)
+            return K.sum(y_true * y_pred, axis=-1)
     def compute_output_shape(self, input_shape):
         return (None, X.shape[1]-1)
 
@@ -43,7 +55,7 @@ class SoftmaxDropout(Layer):
         self.stddev = stddev
 
     def call(self, x, training=None):
-        return x * K.cast(K.greater(x + K.random_normal(shape=K.shape(x),
+        return K.cast(K.greater(x + K.random_normal(shape=K.shape(x),
                                                         mean=self.mean,
                                                         stddev=self.stddev), 0.5), 'float32')
 
@@ -59,12 +71,14 @@ def consciousness_rnn():
     i = Input(shape=(X.shape[1], latent_dim))
     x = GRU(latent_dim, return_sequences = True)(i)
     x = TimeDistributed(Dense(X.shape[2], activation='sigmoid'))(x)
+    xa_probabilities = x
     x = SoftmaxDropout(0.,1.0)(x)
     xa = Reshape([X.shape[1], 32])(x)
     x = TimeDistributed(Dense(X.shape[2], activation='sigmoid'))(x)
+    xb_probabilities = x
     x = SoftmaxDropout(0.,1.0)(x)
     xb = Reshape([X.shape[1], 32])(x)
-    model = Model(inputs=i, outputs=[xa, xb], name='Consciousness')
+    model = Model(inputs=i, outputs=[xa, xb, xa_probabilities, xb_probabilities], name='Consciousness')
     return model
 
 def generator_rnn():
@@ -83,10 +97,10 @@ C = consciousness_rnn()
 G = generator_rnn()
 
 h = R(i) # Get h from R
-c_A, c_B = C(h) # Get masks c_A and c_B from C
+c_A, c_B, c_A_soft, c_B_soft = C(h) # Get masks c_A and c_B from C
 b = multiply([h, c_B], name = 'b') # Get b through elementwise multiplication
 a_hat = G([c_A, c_B, b]) # Send c_A, c_B and b to G to get a_hat
-intelligence_error = concatenate([c_A, c_B]) # The more elements we choose to predict, the more "intelligent" we are
+
 a_hat = Lambda(lambda x: x[:,:-1,:], output_shape=(X.shape[1]-1, latent_dim))(a_hat) # Slice dimensions to align vectors
 h_A = Lambda(lambda x: x[:,1:,:], output_shape=(X.shape[1]-1, latent_dim))(h) # Slice dimensions to align vectors
 c_A = Lambda(lambda x: x[:,:-1,:], output_shape=(X.shape[1]-1, latent_dim))(c_A) # Slice dimensions to align vectors
@@ -95,14 +109,14 @@ h_A = multiply([h_A, c_A]) # Calculate h[A] to compare against a_hat
 a_hat = multiply([a_hat, c_A]) # Mask a_hat
 prediction_error = subtract([a_hat, h_A], name='Prediction_Error')
 
-b_transformed = Dense(latent_dim, activation='linear')(b)
+b_transformed = Dense(latent_dim, activation='linear')(b) # Create a layer that attempts to make b independent from h[A]
 b_transformed = Lambda(lambda x: x[:,:-1,:], output_shape=(X.shape[1]-1, latent_dim))(b_transformed)
+b_transformed = multiply([b_transformed, c_A])
+#transformation_error = CPLayer(name='Transformation_Error')([b_transformed, h_A])
 transformation_error = subtract([b_transformed, h_A], name='Transformation_Error')
-intelligence_error = Flatten(name='Intelligence_Level')(intelligence_error)
-#b = Lambda(lambda x: x[:,:-1,:], output_shape=(X.shape[1]-1, latent_dim))(b)
-#c_B = Lambda(lambda x: x[:,:-1,:], output_shape=(X.shape[1]-1, latent_dim))(c_B)
-#mi_error = CELayer(name='KL_Divergence')([a_hat, h])
 
+intelligence_error = concatenate([c_A_soft, c_B_soft]) # The more elements we choose to predict, the more "intelligent" we are
+intelligence_error = Flatten(name='Intelligence_Level')(intelligence_error)
 
 ## Compile the model and start training
 CN = Model(inputs=i, outputs=[prediction_error, transformation_error, intelligence_error])
